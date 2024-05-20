a439f6b6561c487f0b5d4cc4a1e907a4ddfdd2e1
amarphule <amardeepphule@gmail.com>
2024-05-20 16:22:23 +0530

    file handle module fs

--- file.js
+++ file.js
@@ -1,9 +1,18 @@
 const fs = require("fs");
 
-fs.writeFileSync("./text.txt", "Data from nodejs file");
+// fs.writeFileSync("./text.txt", "Data from nodejs file");
 
-fs.writeFile(
-  "./demo.txt",
-  "Async operation need third parameter cb function",
-  () => {}
+// fs.writeFile(
+//   "./demo.txt",
+//   "Async operation need third parameter cb function",
+//   () => {}
+// );
+
+console.log(fs.readFileSync("./contact.txt", "utf-8"));
+console.log(
+  fs.readFile("./text.txt", "utf-8", (error, data) => {
+    console.log(data);
+  })
 );
+
+fs.mkdir("demoDir/a/b", { recursive: true }, () => {});
