6048a4492499b2b979bc2770abb1a4f076fd8796
amarphule <amardeepphule@gmail.com>
2024-05-20 21:21:27 +0530

    create server log

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,8 +1,17 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, resp) => {
-  console.log("url: ", req.url);
-
+  const { method, url } = req;
+  fs.appendFile(
+    "./server.log",
+    `${new Date().toISOString()} - Request: ${method} ${url}\n`,
+    (err) => {
+      if (err) {
+        console.log("Fail to write log file: ", err);
+      }
+    }
+  );
   resp.end("Hello response from http server");
 });
 
