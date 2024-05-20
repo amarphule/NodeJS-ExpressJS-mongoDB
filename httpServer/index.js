af8ce32851ffe45945b391addeaf5b5041a6799b
amarphule <amardeepphule@gmail.com>
2024-05-20 21:38:44 +0530

    add ip address to log file

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,17 +1,33 @@
 const http = require("http");
 const fs = require("fs");
 
+const getClientIp = (req) => {
+  const xForwordedFor = req.headers["x-forworded-for"];
+
+  let ip = xForwordedFor
+    ? xForwordedFor.split(",")[0].trim()
+    : req.connection.remoteAddress || req.socket.remoteAddress;
+
+  // Check if the IP is an IPv6-mapped IPv4 address
+  if (ip.startsWith("::ffff:")) {
+    ip = ip.substring(7);
+  }
+  return ip;
+};
+
 const server = http.createServer((req, resp) => {
   const { method, url } = req;
+  const clientIp = getClientIp(req);
   fs.appendFile(
     "./server.log",
-    `${new Date().toISOString()} - Request: ${method} ${url}\n`,
+    `${new Date().toISOString()} - Request from ${clientIp}: ${method} ${url}\n`,
     (err) => {
       if (err) {
         console.log("Fail to write log file: ", err);
       }
     }
   );
+
   resp.end("Hello response from http server");
 });
 
