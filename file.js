411d8abc1ad9a08fe55a8998153b7792c28281ee
amarphule <amardeepphule@gmail.com>
2024-05-20 16:10:17 +0530

    created file demo.txt using fs async method

--- file.js
+++ file.js
@@ -1,3 +1,9 @@
 const fs = require("fs");
 
 fs.writeFileSync("./text.txt", "Data from nodejs file");
+
+fs.writeFile(
+  "./demo.txt",
+  "Async operation need third parameter cb function",
+  () => {}
+);
