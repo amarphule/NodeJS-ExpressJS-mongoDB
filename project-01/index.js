51db4cd0398d6fadf5873c640845b1c7a6bf35c6
amarphule <amardeepphule@gmail.com>
2024-05-21 12:11:13 +0530

    fix: post req

--- project-01/index.js
+++ project-01/index.js
@@ -8,4 +8,23 @@ app.get("/users", (req, resp) => {
   return resp.json(users);
 });
 
+// as "/api/users/:id" route is comman for these request we used .route() method
+app
+  .route("/api/users/:id")
+  .get((req, resp) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    resp.json(user);
+  })
+  .patch(() => {
+    // Todo: update user
+  })
+  .delete(() => {
+    // Todo: Delete user
+  });
+
+app.post("/api/users", (req, resp) => {
+  // Todo: Create user
+});
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
