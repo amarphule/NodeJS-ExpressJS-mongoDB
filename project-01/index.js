f3e137efc35a696a4ba2b1551d4e9d177f6402be
amarphule <amardeepphule@gmail.com>
2024-05-21 14:46:54 +0530

    REST api done

--- project-01/index.js
+++ project-01/index.js
@@ -17,18 +17,54 @@ app
   .route("/api/users/:id")
   .get((req, resp) => {
     const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
+    let user = users.find((user) => user.id === id);
+    // Check if the user was found and removed
+    if (!user) {
+      resp.status(404).json({ status: "Error", message: "User not found" });
+      return;
+    }
     resp.json(user);
   })
-  .patch(() => {
-    // Todo: update user
+  .patch((req, resp) => {
+    const id = Number(req.params.id);
+
+    let userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex === -1) {
+      resp.status(404).json({ status: "Error", message: "User not found" });
+      return;
+    }
+
+    const updatedUser = { ...users[userIndex], ...req.body };
+
+    users[userIndex] = updatedUser;
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) throw err;
+      resp.json({ status: "Successfully Update", id });
+    });
   })
-  .delete(() => {
-    // Todo: Delete user
+  .delete((req, resp) => {
+    let initialLength = users.length;
+    const id = Number(req.params.id);
+
+    const updatedUsers = users.filter((user) => user.id !== id);
+
+    // Check if the user was found and removed
+    if (users.length === initialLength) {
+      resp.status(404).json({ status: "Error", message: "User not found" });
+      return;
+    }
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(updatedUsers), (err) => {
+      if (err) throw err;
+      resp.json({ status: "Successfully deleted", id });
+    });
   });
 
 app.post("/api/users", (req, resp) => {
   const body = req.body;
+
   users.push({ id: users.length + 1, ...body });
 
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
