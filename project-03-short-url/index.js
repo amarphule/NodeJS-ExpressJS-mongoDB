dbb407f0b9407debec110efe7a583297ad5542ce
amarphule <amardeepphule@gmail.com>
2024-05-22 15:49:08 +0530

    create mongoDb connection

--- project-03-short-url/index.js
+++ project-03-short-url/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const { dbConnect } = require("./dbConnect");
+
+// Load environment variables from .env file
+require("dotenv").config();
 
 const app = express();
 const PORT = 8000;
+
+dbConnect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to DB"))
+  .catch((error) => console.log("Error connecting to DB: ", error));
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
